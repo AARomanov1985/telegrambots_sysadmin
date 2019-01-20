6e6f157d55ce6dc02355a9a9b3ea5f5ed89098b5
Aleksandr Romanov <AARomanov1985@yandex.ru>
2019-01-20 17:41:09 +0300

    SYSADM-002 Integrate with Telegram API

---
+++ src/main/java/ru/aaromanov1985/telegrambots/simplebot/Bot.java
@@ -0,0 +1,62 @@
+package ru.aaromanov1985.telegrambots.simplebot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    private Logger LOG = LoggerFactory.getLogger(Bot.class);
+    private final static String NAME = "sysadm623_bot";
+    private final static String TOKEN = "727435438:AAGDzmWU0TthV2lScaIP0MDP2f8L_CQtKpk";
+
+    public static void main(String[] args) {
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return TOKEN;
+    }
+}
